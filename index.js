c1acff6e63061b420a973dd6950beb69c16ccc55
Fahad <fahad.b1499@gmail.com>
2019-12-06 10:49:29 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const{Builder, By, Key, Util} = require("selenium-webdriver");
+async function example() {
+    let driver = await new Builder().forBrowser("chrome").build();
+    await driver.get("https://s2.demo.opensourcecms.com/orangehrm/symfony/web/index.php/auth/login");
+    await driver.findElement(By.name("txtUsername")).sendKeys("opensourcecms");
+    await driver.findElement(By.name("txtPassword")).sendKeys("opensourcecms", Key.RETURN);
+        
+}
+example();
